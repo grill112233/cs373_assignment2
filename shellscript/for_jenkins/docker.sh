e2717f14370d39546072dbd822c21f94de9e73a7
grill112233 <grill_0822553047@hotmail.com>
2020-12-27 09:40:40 +0700

    fix shellscript for jenkins V3

--- shellscript/for_jenkins/docker.sh
+++ shellscript/for_jenkins/docker.sh
@@ -2,4 +2,3 @@ cd ..
 cd ..
 cd docker_compose
 docker-compose up --build
-docker-compose stop
--- shellscript/for_jenkins/toTomcat.sh
+++ shellscript/for_jenkins/toTomcat.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ..
+cd ..
 cd target
 mv assignment2-0.0.1-SNAPSHOT.war myweb.war
 docker cp myweb.war tomcat_tomcat-assignment2_1:/usr/local/tomcat/webapps
-docker-compose start
