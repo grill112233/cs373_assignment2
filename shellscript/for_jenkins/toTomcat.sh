11629da2072bd3420c8d68256d083ec185e8a13d
grill112233 <grill_0822553047@hotmail.com>
2020-12-27 10:27:07 +0700

    fix V8

--- shellscript/for_jenkins/toTomcat.sh
+++ shellscript/for_jenkins/toTomcat.sh
@@ -3,4 +3,4 @@ cd ..
 cd ..
 cd target
 mv assignment2-0.0.1-SNAPSHOT.war myweb.war
-docker cp myweb.war tomcat_tomcat-assignment2_1:/usr/local/tomcat/webapps
+docker cp myweb.war docker_compose_tomcat-assignment2_1:/usr/local/tomcat/webapps
