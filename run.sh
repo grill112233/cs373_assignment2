09039de3d21b3b0277201348e392cebb7398023c
grill112233 <grill_0822553047@hotmail.com>
2020-12-27 10:09:31 +0700

    fix run.sh V5

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd shellscript/for_jenkins/
-./docker.sh
+./docker.sh &
+echo "--------- Please run 2 time for project ---------"
+echo "Step 1: for build images and create container."
+echo "Step 2: for copy .war to container."
 cd ..
 cd ..
 cd shellscript/for_jenkins/
