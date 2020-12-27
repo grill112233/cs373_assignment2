8c93702cb6b5e65834aedf34b49a5e444902a4f0
grill112233 <grill_0822553047@hotmail.com>
2020-12-27 10:25:55 +0700

    fix run.sh V7

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cd shellscript/for_jenkins/
-
+echo "--------- Please run 2 time for project ---------"
+echo "Step 1: for build images and create container."
+echo "Step 2: for copy .war to container."
 if [[ "$(docker images tomcat  | grep tomcat  2> /dev/null)" != "" ]]; then
     echo "exist."
     ./toTomcat.sh
